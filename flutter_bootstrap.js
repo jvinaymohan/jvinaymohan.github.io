7b89867ef9df6ddf913c2fe7faf15ff3b42d06e2
jvinaymohan <jvinaymohan@gmail.com>
2025-07-30 23:18:03 -0500

    Enhanced coach features: profile updates, location info, improved class creation with duration, time defaults, and marketplace promotion

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "701487002"
+    serviceWorkerVersion: "616105017"
   }
 });
